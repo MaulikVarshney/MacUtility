77a01f784e085163f4244915864c0d982dfecf1e
Maulik Varshney <maulikvarshney@Mauliks-MacBook-Air.local>
2023-11-02 17:03:31 +0530

    Test Commit

---
+++ src/main/java/org/example/example.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class example {
+    public static void main(String[] args) {
+        System.out.println("Hey I am here and this is my first piece of code.");
+    }
+
+}
