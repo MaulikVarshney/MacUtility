4786c335ae7116ed3228a6eb507165591326c9c7
maulikvarshney <varshneymaulik05@gmail.com>
2023-11-06 23:59:28 +0530

    Another branch commit.

--- src/main/java/org/example/example.java
+++ src/main/java/org/example/example.java
@@ -3,6 +3,7 @@ package org.example;
 public class example {
     public static void main(String[] args) {
         System.out.println("Hey I am here and this is my first piece of code.");
+        System.out.println("Working on a new branch.");
     }
 
 }
